63a905c01f020b5b6ea9599100f6cce74ee6ec14
jay <75509151@qq.com>
2023-02-25 21:59:10 +0800

    添加 covert脚本 js解析

---
+++ iot-components/iot-component-converter/src/main/java/cc/iotkit/converter/GraalJsScriptConverter.java
@@ -0,0 +1,77 @@
+/*
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 奇特物联 2021-2022 All rights reserved.
+ * +----------------------------------------------------------------------
+ * | Licensed 未经许可不能去掉「奇特物联」相关版权
+ * +----------------------------------------------------------------------
+ * | Author: xw2sy@163.com
+ * +----------------------------------------------------------------------
+ */
+package cc.iotkit.converter;
+
+import cc.iotkit.common.thing.ThingService;
+import cc.iotkit.common.utils.JsonUtil;
+import cc.iotkit.model.device.message.ThingModelMessage;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.Map;
+import org.graalvm.polyglot.*;
+
+@Slf4j
+@Data
+public class GraalJsScriptConverter implements IConverter {
+
+
+    private final Context context = Context.newBuilder("js").allowHostAccess(true).build();
+
+    private Object scriptObj;
+
+    private Value decoder;
+    private Value encoder;
+
+    public void setScript(String script) {
+
+        Value myFunctions = context.eval("js",String.format("new (function () {\n%s})()", script));
+        // 调用JavaScript函数
+        decoder = myFunctions.getMember("decode");
+        encoder = myFunctions.getMember("encode");
+
+    }
+
+    public ThingModelMessage decode(DeviceMessage msg) {
+        try {
+//            String msgJson = JsonUtil.toJsonString(msg);
+            Value rst = decoder.execute(msg);
+            ThingModelMessage modelMessage = new ThingModelMessage();
+            CovertUtils.copyProperties(modelMessage, rst);
+            return modelMessage;
+        } catch (Throwable e) {
+            log.error("execute decode script error", e);
+        }
+        return null;
+    }
+
+
+    @Override
+    public DeviceMessage encode(ThingService<?> service, Device device) {
+        try {
+            Value rst = encoder.execute(service,device);
+            DeviceMessage modelMessage = rst.as(DeviceMessage.class);
+            return modelMessage;
+        } catch (Throwable e) {
+            log.error("execute encode script error", e);
+        }
+        return null;
+    }
+
+    @Override
+    public void putScriptEnv(String key, Object value) {
+        context.getBindings("js").putMember(key, value);
+    }
+}
