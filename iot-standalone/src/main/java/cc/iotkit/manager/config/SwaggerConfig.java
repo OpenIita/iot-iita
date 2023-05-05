668a26ddbf358c812e88e9799489002ea0d59d0c
荭琪枫 <374696376@qq.com>
2023-05-05 22:42:46 +0800

    add swagger support

--- iot-standalone/src/main/java/cc/iotkit/manager/config/SwaggerConfig.java
+++ iot-standalone/src/main/java/cc/iotkit/manager/config/SwaggerConfig.java
@@ -4,14 +4,15 @@ import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: 石恒
@@ -30,7 +31,10 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalRequestParameters(getGlobalRequestParameters())
+                .globalResponses(HttpMethod.GET, getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST, getGlobalResponseMessage());
     }
 
     @Bean
@@ -41,6 +45,25 @@ public class SwaggerConfig {
                 .contact(new Contact("奇特物联-开源", "https://gitee.com/iotkit-open-source/iotkit-parent.git", "user@iotkit.com"))
                 .version("1.0")
                 .build();
+    }
+
+    /**
+     * 添加head参数配置
+     */
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("token")
+                .description("令牌")
+                .required(false)
+                .in(ParameterType.HEADER)
+                .build());
+        return parameters;
+    }
+    private List<Response> getGlobalResponseMessage() {
+        List<Response> responseList = new ArrayList<>();
+        responseList.add(new ResponseBuilder().code("404").description("找不到资源").build());
+        return responseList;
     }
 
 }
