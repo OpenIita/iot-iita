6fc65a774b2958d5261c97c9b96b2bcdf937ccd1
荭琪枫 <374696376@qq.com>
2023-05-16 23:35:41 +0800

    update:修改通道配置显示问题

--- iot-data/iot-model/src/main/java/cc/iotkit/model/notify/ChannelConfig.java
+++ iot-data/iot-model/src/main/java/cc/iotkit/model/notify/ChannelConfig.java
@@ -1,6 +1,7 @@
 package cc.iotkit.model.notify;
 
 import cc.iotkit.model.Id;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,6 +29,7 @@ public class ChannelConfig implements Id<String> {
     private Long createAt;
 
     @Data
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class ChannelParam {
         private String userName;
         private String passWord;
