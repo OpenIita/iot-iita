72dd4a9a0ce078d3c3134c6917a761b9f7a0c9c3
tangfudong <280620913@qq.com>
2023-06-01 09:52:03 +0800

    update:多租户待定

--- iot-common/iot-common-dao/iot-data-serviceImpl-rdb/src/main/java/cc/iotkit/data/service/SysTenantDataImpl.java
+++ iot-common/iot-common-dao/iot-data-serviceImpl-rdb/src/main/java/cc/iotkit/data/service/SysTenantDataImpl.java
@@ -1,12 +1,21 @@
 package cc.iotkit.data.service;
 
 import cc.iotkit.common.utils.MapstructUtils;
+import cc.iotkit.common.utils.StringUtils;
 import cc.iotkit.data.dao.SysTenantRepository;
+import cc.iotkit.data.model.TbSysTenant;
 import cc.iotkit.data.system.ISysTenantData;
-import cc.iotkit.model.system.SysUser;
+import cc.iotkit.data.util.PredicateBuilder;
+import cc.iotkit.model.system.SysTenant;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+import static cc.iotkit.data.model.QTbSysTenant.tbSysTenant;
 
 /**
  * @Author：tfd
@@ -19,8 +28,51 @@ public class SysTenantDataImpl implements ISysTenantData {
 
     private SysTenantRepository sysTenantRepository;
 
+    private final JPAQueryFactory jpaQueryFactory;
+
     @Override
-    public SysUser findById(Long aLong) {
-        return MapstructUtils.convert(sysTenantRepository.findById(aLong),SysUser.class);
+    public SysTenant findById(Long aLong) {
+        return MapstructUtils.convert(sysTenantRepository.findById(aLong),SysTenant.class);
+    }
+
+    @Override
+    public SysTenant save(SysTenant data) {
+        return null;
+    }
+
+    @Override
+    public void batchSave(List<SysTenant> data) {
+
+    }
+
+    @Override
+    public long deleteById(Long aLong) {
+
+        return 0;
+    }
+
+    @Override
+    public int deleteByIds(Collection<Long> longs) {
+
+        return 0;
+    }
+
+    @Override
+    public List<SysTenant> findAllByCondition(SysTenant data) {
+        List<TbSysTenant> ret=jpaQueryFactory.selectFrom(tbSysTenant).where(PredicateBuilder.instance()
+                .and(StringUtils.isNotBlank(data.getTenantId()),()->tbSysTenant.tenantId.eq(data.getTenantId()))
+                .and(StringUtils.isNotBlank(data.getContactUserName()),()->tbSysTenant.contactUserName.like(data.getContactUserName()))
+                .and(StringUtils.isNotBlank(data.getContactPhone()),()->tbSysTenant.contactPhone.eq(data.getContactPhone()))
+                .and(StringUtils.isNotBlank(data.getCompanyName()),()->tbSysTenant.companyName.like(data.getCompanyName()))
+                .and(StringUtils.isNotBlank(data.getLicenseNumber()),()->tbSysTenant.licenseNumber.eq(data.getLicenseNumber()))
+                .and(StringUtils.isNotBlank(data.getAddress()),()->tbSysTenant.address.eq(data.getAddress()))
+                .and(StringUtils.isNotBlank(data.getIntro()),()->tbSysTenant.intro.eq(data.getIntro()))
+                .and(StringUtils.isNotBlank(data.getDomain()),()->tbSysTenant.domain.like(data.getDomain()))
+                .and(data.getPackageId() != null,()->tbSysTenant.packageId.eq(data.getPackageId()))
+                .and(data.getExpireTime() != null,()->tbSysTenant.expireTime.eq(data.getExpireTime()))
+                .and(data.getAccountCount() != null,()->tbSysTenant.accountCount.eq(data.getAccountCount()))
+                .and(StringUtils.isNotBlank(data.getStatus()),()->tbSysTenant.status.eq(data.getStatus()))
+                .build()).fetch();
+        return MapstructUtils.convert(ret, SysTenant.class);
     }
 }
